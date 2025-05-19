3f1cd3f00cf1cdf70d8ebad15048ac771e9eeb86
EfrainXD <efrainsanchezgarcia9@gmail.com>
2025-05-19 20:15:09 +0200

    Corregido

--- src/main/java/com/efragame/App.java
+++ src/main/java/com/efragame/App.java
@@ -12,7 +12,8 @@ public class App extends Application {
         SceneManager.getInstance().init(primaryStage);
 
         FXMLLoader cargar = new FXMLLoader();
-        cargar.setLocation(getClass().getResource("/com/efraigame/vistas/VistaEstadisticas.fxml"));
+        cargar.setLocation(getClass().getResource("/com/efragame/vistas/VistaEstadisticas.fxml"
+));
         Parent root = cargar.load();
 
         Scene escena = new Scene(root);
